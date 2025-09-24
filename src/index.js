386d69e1293eea33ecec7ee7ca65d6b259a852ba
Muskan Kumar <muskankumar952593@gmail.com>
2025-09-24 23:28:33 +0530

    add controller and routes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 // require('dotenv').config({path:'./env'})
-import express from 'express'
+// import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
-const app=express()
+import { app } from './app.js'
+const PORT=process.env.PORT || 8000
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at port ${process.env.PORT}`)
-    })
+    app.listen(PORT,()=>console.log(`Server is running at port ${process.env.PORT}`))
 })
 .catch((err)=>{
     console.log("MONGO DB connection failed !!!",err)
---
+++ src/models/user.model.js
@@ -0,0 +1,91 @@
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const userSchema=new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+        },
+        avatar:{
+            type:String,    //cloudinary url
+            required:true,
+        },
+        coverImage:{
+            type:String,    //cloudinary url
+        },
+        watchHistory:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Video"
+        },
+        password:{
+            type:String,
+            required:[true,'Password is required']
+        },
+        refreshToken:{
+            type:String
+        }
+    },
+    {timestamps:true}
+)
+
+//save krne se pahle password hash kr dega
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password=await bcrypt.hash(this.password,10)
+    next();
+})
+
+//compare krega ki save password and access krne ke liye user jo password de rha h same h ya nhi
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+//token generate krne ke liye(access token)
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this.id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+//token generate krne ke liye(refresh token)
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+            _id:this.id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User=mongoose.model("User",userSchema)
